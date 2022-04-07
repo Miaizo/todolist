cf55d62dcb1fa3cafa889c8779a052d6694a74f9
wx <miaizo@sina.com>
2022-04-07 18:02:08 +0800

    feat: init

---
+++ src/components/Hello/Hello.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Hello.css'
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <h1>
+        <span className='title'>hello</span>
+      </h1>
+    )
+  }
+}
